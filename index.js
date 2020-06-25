27546da12118961c5c7dd358caf3fd3d51bba5bd
swimauger <swimauger@gmail.com>
2020-06-24 22:31:21 -0500

    Add Image Classifier

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ImageClassifier: require('./lib/ImageClassifier'),
+    Dataset: require('./lib/Dataset'),
+    parseCanvas: require('./lib/parseCanvas'),
+    version: require('./package.json').version
+}
